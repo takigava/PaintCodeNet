fe7770794f3b45c9fe8f1e077ea4c839c9d34c25
takigava <tired_heart@mail.ru>
2016-06-01 13:19:26 +0300

    First commit

---
+++ PaintCodeNet.Models/Variables/VariableItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintCodeNet.Models.Variables
+{
+    public interface IField
+    {
+
+    }
+    public class VariableItem<T> : IField
+    {
+        public Type Type
+        {
+            get
+            {
+                return typeof(T);
+            }
+        }
+        public VariableType VarType { get; set; }
+        public InputType InType { get; set; }
+        public T Variable { get; set; }
+    }
+}
---
+++ PaintCodeNet/Views/Inspector.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Syncfusion.Windows.Shared;
+
+namespace PaintCodeNet.Views
+{
+    /// <summary>
+    /// Interaction logic for Inspector.xaml
+    /// </summary>
+    public partial class Inspector : UserControl
+    {
+        public Inspector()
+        {
+            
+            InitializeComponent();
+        }
+    }
+}
